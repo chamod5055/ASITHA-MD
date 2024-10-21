35199493d3eec2ad265efda33ea10ac70d8d6cb9
chamod5055 <cryptoboysonalchamod23947950@gmail.com>
2024-10-21 15:32:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=6uohmAxL#uCHOIzGce3xahQ1ErXIt83R7g1MrV9rHkxDGWvPKorw",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=O2gDFLYI#tndgnWFIGPKn4IecjlwuJEucZLG0-XN499idzoZBk5g",
 MONGODB: process.env.MONGODB || "mongodb+srv://chamodsadasaragithub215055:XT6EnloAMb8r8Aly@cluster0.r6rgu.mongodb.net/",//enter mongo db url
 };
